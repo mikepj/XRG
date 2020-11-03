b5e7605b25343bb23d750c46bfce291fca67a351
Mike Piatek-Jimenez <mike@gauchosoft.com>
2020-11-03 13:46:14 -0500

    Add a stats manager to keep track of min/max values.

---
+++ Utility/XRGStatsManager.h
@@ -0,0 +1,65 @@
+//
+//  XRGStatsManager.h
+//  XRG
+//
+//  Created by Mike Piatek-Jimenez on 11/3/20.
+//  Copyright © 2020 Gaucho Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "XRGDataSet.h"
+
+typedef NS_ENUM(NSInteger, XRGStatsModule) {
+    XRGStatsModuleNameCPU,
+    XRGStatsModuleNameGPU,
+    XRGStatsModuleNameMemory,
+    XRGStatsModuleNameTemperature,
+    XRGStatsModuleNameBattery,
+    XRGStatsModuleNameDisk,
+    XRGStatsModuleNameNetwork,
+    XRGStatsModuleNameWeather,
+    XRGStatsModuleNameStock
+};
+
+NS_ASSUME_NONNULL_BEGIN
+
+#pragma mark - XRGStatsContentItem
+@interface XRGStatsContentItem: NSObject
+
+@property NSString *key;
+
+@property double last;
+@property double min;
+@property double max;
+
+- (instancetype)initWithKey:(NSString *)key initialValue:(double)initialValue;
+- (void)observeStat:(double)value;
+
+@end
+
+
+#pragma mark - XRGStatsModuleContent
+@interface XRGStatsModuleContent: NSObject
+
+- (NSArray<NSString *> *)keys;
+
+- (nullable XRGStatsContentItem *)statsForKey:(NSString *)key;
+- (void)observeStat:(double)value forKey:(NSString *)key;
+
+@end
+
+
+#pragma mark - XRGStatsManager
+@interface XRGStatsManager : NSObject
+
++ (instancetype)shared;
+
+- (void)clearHistory;
+- (void)clearHistoryForModule:(XRGStatsModule)module;
+
+- (nullable XRGStatsContentItem *)statForKey:(NSString *)key inModule:(XRGStatsModule)module;
+- (void)observeStat:(double)value forKey:(NSString *)key inModule:(XRGStatsModule)module;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Utility/XRGStatsManager.m
@@ -0,0 +1,182 @@
+//
+//  XRGStatsManager.m
+//  XRG
+//
+//  Created by Mike Piatek-Jimenez on 11/3/20.
+//  Copyright © 2020 Gaucho Software. All rights reserved.
+//
+
+#import "XRGStatsManager.h"
+
+#pragma mark - XRGStatsManager
+@interface XRGStatsManager ()
+
+@property NSDictionary<NSString *, XRGStatsModuleContent *> *modulesContent;
+
+@end
+
+@implementation XRGStatsManager
+
++ (NSString *)nameOfModule:(XRGStatsModule)module {
+    switch (module) {
+        case XRGStatsModuleNameCPU:
+            return @"XRG_CPU";
+
+        case XRGStatsModuleNameGPU:
+            return @"XRG_GPU";
+
+        case XRGStatsModuleNameMemory:
+            return @"XRG_Memory";
+
+        case XRGStatsModuleNameTemperature:
+            return @"XRG_Temperature";
+
+        case XRGStatsModuleNameBattery:
+            return @"XRG_Battery";
+
+        case XRGStatsModuleNameDisk:
+            return @"XRG_Disk";
+
+        case XRGStatsModuleNameNetwork:
+            return @"XRG_Network";
+
+        case XRGStatsModuleNameWeather:
+            return @"XRG_Weather";
+
+        case XRGStatsModuleNameStock:
+            return @"XRG_Stock";
+            
+    }
+}
+
++ (instancetype)shared {
+    static XRGStatsManager *sharedManager = nil;
+
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedManager = [[XRGStatsManager alloc] init];
+    });
+
+    return sharedManager;
+}
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        self.modulesContent = [NSDictionary dictionary];
+    }
+    return self;
+}
+
+- (void)clearHistory {
+    self.modulesContent = [NSDictionary dictionary];
+}
+
+- (void)clearHistoryForModule:(XRGStatsModule)module {
+    NSString *moduleName = [XRGStatsManager nameOfModule:module];
+
+    NSMutableDictionary *newStats = [self.modulesContent mutableCopy];
+    [newStats removeObjectForKey:moduleName];
+    self.modulesContent = newStats;
+}
+
+- (XRGStatsContentItem *)statForKey:(NSString *)key inModule:(XRGStatsModule)module {
+    return [[self contentForModule:module] statsForKey:key];
+}
+
+- (void)observeStat:(double)value forKey:(NSString *)key inModule:(XRGStatsModule)module {
+    [[self contentForModule:module] observeStat:value forKey:key];
+}
+
+- (XRGStatsModuleContent *)contentForModule:(XRGStatsModule)module {
+    NSString *moduleName = [XRGStatsManager nameOfModule:module];
+
+    XRGStatsModuleContent *content = self.modulesContent[moduleName];
+    if (content) return content;
+
+    XRGStatsModuleContent *newContent = [[XRGStatsModuleContent alloc] init];
+
+    NSMutableDictionary *newModulesContent = [self.modulesContent mutableCopy];
+    newModulesContent[moduleName] = newContent;
+    self.modulesContent = newModulesContent;
+
+    return newContent;
+}
+
+@end
+
+
+#pragma mark - XRGStatsModuleContent
+@interface XRGStatsModuleContent ()
+
+@property NSDictionary<NSString *, XRGStatsContentItem *> *items;
+
+@end
+
+@implementation XRGStatsModuleContent: NSObject
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        self.items = [NSDictionary dictionary];
+    }
+    return self;
+}
+
+- (NSArray<NSString *> *)keys {
+    return self.items.allKeys;
+}
+
+- (nullable XRGStatsContentItem *)statsForKey:(NSString *)key {
+    return self.items[key];
+}
+
+- (void)observeStat:(double)value forKey:(NSString *)key {
+    XRGStatsContentItem *item = [self statItemForKey:key initialValue:value];
+
+    [item observeStat:value];
+}
+
+#pragma mark Private
+- (XRGStatsContentItem *)statItemForKey:(NSString *)key initialValue:(double)initialValue {
+    XRGStatsContentItem *existingItem = self.items[key];
+    if (existingItem) return existingItem;
+
+    XRGStatsContentItem *newItem = [[XRGStatsContentItem alloc] initWithKey:key initialValue:initialValue];
+
+    NSMutableDictionary *newItems = [self.items mutableCopy];
+    newItems[key] = newItem;
+    self.items = newItems;
+
+    return newItem;
+}
+
+@end
+
+
+#pragma mark - XRGStatsContentItem
+@implementation XRGStatsContentItem
+
+- (instancetype)initWithKey:(NSString *)key initialValue:(double)initialValue {
+    self = [super init];
+    if (self) {
+        self.key = key;
+        self.last = initialValue;
+        self.min = initialValue;
+        self.max = initialValue;
+    }
+    return self;
+}
+
+- (void)observeStat:(double)value {
+    if (value < self.min) {
+        self.min = value;
+    }
+    if (value > self.max) {
+        self.max = value;
+    }
+
+    self.last = value;
+}
+
+@end
