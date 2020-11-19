8bcfc4811b64ae645420d972fc56406bc15eda47
Mike Piatek-Jimenez <mike@gauchosoft.com>
2020-11-18 23:00:39 -0500

    Put the sensors into a scrollview so the sensor window can be smaller.

---
+++ Utility/XRGFlippedView.h
@@ -0,0 +1,17 @@
+//
+//  XRGFlippedView.h
+//  XRG
+//
+//  Created by Mike Piatek-Jimenez on 11/18/20.
+//  Copyright © 2020 Gaucho Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XRGFlippedView : NSView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Utility/XRGFlippedView.m
@@ -0,0 +1,17 @@
+//
+//  XRGFlippedView.m
+//  XRG
+//
+//  Created by Mike Piatek-Jimenez on 11/18/20.
+//  Copyright © 2020 Gaucho Software. All rights reserved.
+//
+
+#import "XRGFlippedView.h"
+
+@implementation XRGFlippedView
+
+- (BOOL)isFlipped {
+    return YES;
+}
+
+@end
