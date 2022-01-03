c289d23631c93326050167c9a4a84a5e6364d557
Mike Piatek-Jimenez <mike@gauchosoft.com>
2022-01-03 10:19:48 -0800

    Adjust byte string formatting slightly.

--- Utility/XRGCommon.m
+++ Utility/XRGCommon.m
@@ -42,25 +42,25 @@
 
 + (NSString *)formattedStringForBytes:(double)bytes {
     if (bytes >= 112589990684262400.)
-        return [NSString stringWithFormat:@"%3.1fP", (bytes / 1125899906842624.)];
+        return [NSString stringWithFormat:@"%.1fP", (bytes / 1125899906842624.)];
     else if (bytes >= 1125899906842624.)
-        return [NSString stringWithFormat:@"%3.2fP", (bytes / 1125899906842624.)];
+        return [NSString stringWithFormat:@"%.2fP", (bytes / 1125899906842624.)];
     else if (bytes >= 109951162777600.)
-        return [NSString stringWithFormat:@"%3.1fT", (bytes / 1099511627776.)];
+        return [NSString stringWithFormat:@"%.1fT", (bytes / 1099511627776.)];
     else if (bytes >= 1099511627776.)
-        return [NSString stringWithFormat:@"%3.2fT", (bytes / 1099511627776.)];
+        return [NSString stringWithFormat:@"%.2fT", (bytes / 1099511627776.)];
     else if (bytes >= 107374182400.)
-        return [NSString stringWithFormat:@"%3.1fG", (bytes / 1073741824.)];
+        return [NSString stringWithFormat:@"%.1fG", (bytes / 1073741824.)];
     else if (bytes >= 1073741824.)
-        return [NSString stringWithFormat:@"%3.2fG", (bytes / 1073741824.)];
+        return [NSString stringWithFormat:@"%.2fG", (bytes / 1073741824.)];
     else if (bytes >= 104857600.)
-        return [NSString stringWithFormat:@"%3.1fM", (bytes / 1048576.)];
+        return [NSString stringWithFormat:@"%.1fM", (bytes / 1048576.)];
     else if (bytes >= 1048576.)
-        return [NSString stringWithFormat:@"%3.2fM", (bytes / 1048576.)];
+        return [NSString stringWithFormat:@"%.2fM", (bytes / 1048576.)];
     else if (bytes >= 102400.)
-        return [NSString stringWithFormat:@"%4.0fK", (bytes / 1024.)];
+        return [NSString stringWithFormat:@"%.0fK", (bytes / 1024.)];
     else if (bytes >= 1024.)
-        return [NSString stringWithFormat:@"%4.1fK", (bytes / 1024.)];
+        return [NSString stringWithFormat:@"%.1fK", (bytes / 1024.)];
     else
         return [NSString stringWithFormat:@"%ldB", (long)bytes];
 }
